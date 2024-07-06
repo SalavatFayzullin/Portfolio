4cf66bd13a9457530d2081a7849de2233285c868
Salavat Faizullin <s.faizullin@innopolis.university>
2024-07-06 19:06:20 +0300

    Fifth commit

--- app/why_innopolis/page.tsx
+++ app/why_innopolis/page.tsx
@@ -8,9 +8,9 @@ export default function AboutInnopolis() {
     return <>
         <Header></Header>
         <Title text={"Unique Focus on Innovation and Technology"} />
-        <ImageText src={'/img/why1.png'}>Innopolis University is uniquely positioned within Russia innovation ecosystem, focusing on technology and innovation. This specialization can provide you with advanced knowledge and practical skills in cutting-edge technologies, preparing you for careers in rapidly evolving industries.</ImageText>
+        <ImageText src={'/Portfolio/img/why1.png'}>Innopolis University is uniquely positioned within Russia innovation ecosystem, focusing on technology and innovation. This specialization can provide you with advanced knowledge and practical skills in cutting-edge technologies, preparing you for careers in rapidly evolving industries.</ImageText>
         <Title text={"International Recognition and Partnerships"} />
-        <ImageText src={'/img/why2.png'}>Innopolis University collaborates with leading international universities and tech companies, offering exchange programs, joint degrees, and internships. This global network can enhance your educational experience, exposing you to diverse perspectives and opportunities.</ImageText>
+        <ImageText src={'/Portfolio/img/why2.png'}>Innopolis University collaborates with leading international universities and tech companies, offering exchange programs, joint degrees, and internships. This global network can enhance your educational experience, exposing you to diverse perspectives and opportunities.</ImageText>
         <Footer />
     </>
 }

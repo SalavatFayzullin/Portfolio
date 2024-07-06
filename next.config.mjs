1de07bf3a3cd647062118c4ac603e4af9b8685b5
Salavat Faizullin <s.faizullin@innopolis.university>
2024-07-06 18:50:01 +0300

    Fifth commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,3 +3,6 @@ export const reactStrictMode = true
 export const basePath = '/Portfolio'
 export const assetPrefix = '/Portfolio'
 export const output = 'export'
+export const images = {
+  unoptimtized: true
+}

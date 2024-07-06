0dda6b515a13bc2b78fece71ab910bf051f74a0a
Salavat Faizullin <s.faizullin@innopolis.university>
2024-07-06 18:22:19 +0300

    Fifth commit

--- app/about_innopolis/page.tsx
+++ app/about_innopolis/page.tsx
@@ -8,7 +8,7 @@ export default function AboutInnopolis() {
     return <>
         <Header></Header>
         <Title text={"Who are you... Innopolis?"} />
-        <ImageText src={'/img/universitet-innopolis.jpeg'}>Innopolis University is a Russian autonomous non—profit organization of higher education in the city of Innopolis (Verkhneuslonsky district, Republic of Tatarstan) specializing in education, research and development in the field of information technology and robotics.
+        <ImageText src={'/Portfolio/img/universitet-innopolis.jpeg'}>Innopolis University is a Russian autonomous non—profit organization of higher education in the city of Innopolis (Verkhneuslonsky district, Republic of Tatarstan) specializing in education, research and development in the field of information technology and robotics.
 
             Initially, the university operated in the city of Kazan, and since September 2015, the university campus has been opened in Innopolis. Innopolis University trains developers and managers in the field of high technologies and conducts scientific research in the fields of information technology and robotics.
 
--- app/inno_preparation/page.tsx
+++ app/inno_preparation/page.tsx
@@ -8,7 +8,7 @@ export default function AboutInnopolis() {
     return <>
         <Header></Header>
         <Title text={"But how can I prepare to applying?"} />
-        <ImageText src={'/img/inno_prep1.jpg'}>Preparing to apply to Innopolis University involves thorough research, academic preparation, gathering necessary documents, a well-crafted application, and possibly interviews. Additionally, considering financial aspects and staying informed post-application are crucial steps in the process. Remember, each application is unique, so tailor your approach based on the specific requirements and opportunities provided by Innopolis University.</ImageText>
+        <ImageText src={'/Portfolio/img/inno_prep1.jpg'}>Preparing to apply to Innopolis University involves thorough research, academic preparation, gathering necessary documents, a well-crafted application, and possibly interviews. Additionally, considering financial aspects and staying informed post-application are crucial steps in the process. Remember, each application is unique, so tailor your approach based on the specific requirements and opportunities provided by Innopolis University.</ImageText>
         <Footer />
     </>
 }

78022e3db4e1aca52d70edf0ded57906b322639b
Salavat Faizullin <s.faizullin@innopolis.university>
2024-07-06 20:20:33 +0300

    Fifth assignment

--- app/Joke.tsx
+++ app/Joke.tsx
@@ -3,22 +3,22 @@ import { Title } from "./Title"
 import Image from "next/image"
 
 interface JokeProps {
-    title: string | undefined,
-    daysAgo: string | undefined,
-    date: string | undefined,
-    src: string,
-    alt: string
-  }
-  
+  title: string | undefined,
+  daysAgo: string | undefined,
+  date: string | undefined,
+  src: string,
+  alt: string
+}
+
 export const Joke = (props: JokeProps) => (
-    <div id="hw2">
-      <Title text="Joke" />
-      <h2>{props.title}</h2>
-      <h2>{props.daysAgo}</h2>
-      <h2>{props.date}</h2>
-      <div id="joke-container">
-        <img src={props.src} alt={props.alt} />
-        
-      </div>
+  <div id="hw2">
+    <Title text="Joke" />
+    <h2>{props.title}</h2>
+    <h2>{props.daysAgo}</h2>
+    <h2>{props.date}</h2>
+    <div id="joke-container">
+      <img src={props.src} alt={props.alt} />
+
     </div>
-  )
+  </div>
+)
